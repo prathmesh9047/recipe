6fba715aa9c92a11dc215eabb3ab24ff8031a090
prathmesh govalkar <prathmesh.govalkar91@gmail.com>
2018-04-04 17:55:19 +0530

    CRUD Operation - Phase I (Command and Converter)

---
+++ recipe/src/main/java/org/prathmesh/recipe/converters/CategoryCommandToCategory.java
@@ -0,0 +1,28 @@
+package org.prathmesh.recipe.converters;
+
+import org.prathmesh.recipe.commands.CategoryCommand;
+import org.prathmesh.recipe.domain.Category;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import lombok.Synchronized;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+	
+	@Synchronized
+	@Nullable
+	@Override
+	public Category convert(CategoryCommand source){
+		if(source == null){
+			return null;
+		}
+		
+		final Category category = new Category();
+		category.setId(source.getId());
+		category.setDescription(source.getDescription());
+		return category;
+	}
+
+}
---
+++ recipe/src/main/java/org/prathmesh/recipe/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,29 @@
+package org.prathmesh.recipe.converters;
+
+import org.prathmesh.recipe.commands.UnitOfMeasureCommand;
+import org.prathmesh.recipe.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
+}
